0174e20f677008d2496943861bf66f13a7ef2288
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-09-03 22:19:59 +0900

    初期設定

---
+++ src/background.js
@@ -0,0 +1,25 @@
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'loading' && tab.active) {
+
+      // XMLHttpRequestオブジェクトの作成
+        var request = new XMLHttpRequest();
+
+    // URLを開く
+        request.open('GET', "https://api.chucknorris.io/jokes/random", true);
+
+    // レスポンスが返ってきた時の処理を記述 
+        request.onload = function () {
+    // レスポンスが返ってきた時の処理
+        var data = this.response;
+        var obj = JSON.parse(data);
+        console.log(obj.value);
+}
+
+// リクエストをURLに送信
+request.send();
+  
+    }
+    console.log(tabId);
+    console.log(changeInfo);
+    console.log(tab);
+  })
