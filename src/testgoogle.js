358461a70b90c0324a03cbf585a20ea8a56daf34
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-09-04 23:53:44 +0900

    implement sweetalert

---
+++ src/testgoogle.js
@@ -0,0 +1,13 @@
+
+$(function() {
+    console.log("コンテントスクリプトだよ");
+    // $('body').css('background-color','orange');
+    Swal.fire({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
+  });
