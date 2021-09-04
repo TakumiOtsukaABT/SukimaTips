ef1cd16a5b790f21308f9c7977bb8358c2b47b13
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-09-04 17:48:03 +0900

    いったん

--- src/chromeMenu/chromeMenu.js
+++ src/chromeMenu/chromeMenu.js
@@ -1,9 +1,9 @@
-let testButton = document.getElementById("test");
-testButton.addEventListener('click',save);
+// let testButton = document.getElementById("test");
+// testButton.addEventListener('click',save);
 
-function save() {
-    chrome.windows.create( "../ModeLess/modeLess.html", "aaaa",'width=500,height=300,toolbar=yes,menubar=yes,scrollbars=yes');
-}
+// function save() {
+//     ( "../ModeLess/modeLess.html", "aaaa",'width=500,height=300,toolbar=yes,menubar=yes,scrollbars=yes');
+// }
 
 // $("#open-sample-dialog").on("click", function(){
 //   $("#sample-dialog").dialog();
