070de7490b9c8a73c9dafda24c0d60e10e415dfc
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-09-05 12:17:59 +0900

    add on off function

--- src/showSweetAlert.js
+++ src/showSweetAlert.js
@@ -1,14 +1,17 @@
 $(function() {
     chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
         nemui = response.farewell;
-        Swal.fire({
-            iconHtml: '<img src="https://raw.githubusercontent.com/TakumiOtsukaABT/SukimaTips/main/src/artboard.png" width="80" height="80">'
-            , text : nemui
-            , type : 'success'
-            , toast: true
-            , position: 'top-end'   //画面右上
-            , showConfirmButton: false
-            , timer: 5000           //5秒経過後に閉じる
-            });
+        var kidou = response.kidou;
+        if(kidou){
+            Swal.fire({
+                iconHtml: '<img src="https://raw.githubusercontent.com/TakumiOtsukaABT/SukimaTips/main/src/artboard.png" width="80" height="80">'
+                , text : nemui
+                , type : 'success'
+                , toast: true
+                , position: 'top-end'   //画面右上
+                , showConfirmButton: false
+                , timer: 3000           //5秒経過後に閉じる
+                });
+        }
       });
 });
